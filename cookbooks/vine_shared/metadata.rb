fb5d4fc0d4604ccf5f4b9ec79f2ee12ff6ccb4f4
Steven Lehrburger <lehrburger@gmail.com>
2012-12-30 18:00:54 -0500

    Update metadata file with basic mysql info

--- cookbooks/vine_shared/metadata.rb
+++ cookbooks/vine_shared/metadata.rb
@@ -1,6 +1,10 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Vine.IM"
+maintainer_email "lehrburger@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures vine_shared"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+#NOTE using specific versions so that I can stay aware of changes in upstream cookbooks
+depends "mysql", "= 2.1.0"
+depends "database", "= 1.3.9"  #NOTE this is my local version bump of the database cookbook because of http://tickets.opscode.com/browse/COOK-2117
