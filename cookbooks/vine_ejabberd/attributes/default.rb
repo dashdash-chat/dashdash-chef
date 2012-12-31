1a8355e7f49738b81022c5d94a24f6edfb961bc9
Steven Lehrburger <lehrburger@gmail.com>
2012-12-31 17:14:04 -0500

    Install ejabberd modules

--- cookbooks/vine_ejabberd/attributes/default.rb
+++ cookbooks/vine_ejabberd/attributes/default.rb
@@ -1,1 +1,3 @@
 default['vine_ejabberd']['ejabberd_repo_dir'] = "#{Chef::Environment.load(node.chef_environment).default_attributes['source_dir']}/ejabberd"
+default['vine_ejabberd']['modules_repo_dir']  = "#{Chef::Environment.load(node.chef_environment).default_attributes['source_dir']}/ejabberd-modules"
+default['vine_ejabberd']['ejabberd_dir'] = "/lib/ejabberd"
--- cookbooks/vine_ejabberd/recipes/default.rb
+++ cookbooks/vine_ejabberd/recipes/default.rb
@@ -21,7 +21,7 @@ env_data = data_bag_item("dev_data", "dev_data")
   end
 end
 
-# Downdload and install ejabberd
+# Downdload and install ejabberd, then make sure it runs
 git "#{node['vine_ejabberd']['ejabberd_repo_dir']}" do
   repository "https://github.com/lehrblogger/ejabberd.git"
   # use this HTTP URL, since the SSH URL requires a deploy key
@@ -35,3 +35,46 @@ execute "./configure, make, and install ejabberd" do
   cwd "#{node['vine_ejabberd']['ejabberd_repo_dir']}/src"
   action :run
 end
+execute "start ejabberd" do
+  command "sudo ejabberdctl start"
+  action :run
+end
+
+# Download and install the modules, and restart ejabberd
+git "#{node['vine_ejabberd']['modules_repo_dir']}" do
+  repository "https://github.com/lehrblogger/ejabberd-modules.git"
+  branch "master"
+  destination "#{node['vine_ejabberd']['modules_repo_dir']}"
+  action :sync
+end
+["mod_admin_extra",
+ "ejabberd_xmlrpc",
+].each do |module_name|
+  execute "build #{module_name} module for ejabberd" do
+    command "./build.sh"
+    cwd "#{node['vine_ejabberd']['modules_repo_dir']}/#{module_name}/trunk"
+    action :run
+  end
+  execute "install #{module_name} module for ejabberd" do
+    command "cp ebin/#{module_name}.beam #{node['vine_ejabberd']['ejabberd_dir']}/ebin/"
+    cwd "#{node['vine_ejabberd']['modules_repo_dir']}/#{module_name}/trunk"
+    action :run
+  end
+end
+remote_file "/tmp/xmlrpc-1.13-ipr2.tgz" do
+  source "http://ejabberd.jabber.ru/files/contributions/xmlrpc-1.13-ipr2.tgz"
+  notifies :run, "bash[install_xmlrpc_erlang]", :immediately
+end
+bash "install_xmlrpc_erlang" do
+  cwd "/tmp"
+  code <<-EOH
+    tar -xzvf xmlrpc-1.13-ipr2.tgz
+    (cd xmlrpc-1.13/src && make && cp ../ebin/*.beam #{node['vine_ejabberd']['ejabberd_dir']}/ebin/)
+  EOH
+  action :nothing
+end
+execute "restart ejabberd" do
+  command "sudo ejabberdctl restart"
+  action :run
+end
+
