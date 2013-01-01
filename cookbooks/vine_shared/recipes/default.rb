bc6642c1a025f371ff79c42996c83644dfe97f3b
Steven Lehrburger <lehrburger@gmail.com>
2013-01-01 18:22:48 -0500

    Move nginx to vine_shared with optional app_server
    The nginx.conf file is now in vine_shared, and expects the app server from vine_web, but doesn't require it, so it can serve only /supervisor URLs if we need it to for the vine-leaves box.
    
    I also simplifed moved the SSL certs to here, and simplified how they were rendered.
    
    Also, move the .bash_history commands to here from vine_web.

--- cookbooks/vine_shared/recipes/default.rb
+++ cookbooks/vine_shared/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: vine_shared
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Vine.IM
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -11,6 +11,7 @@ env_data = data_bag_item("dev_data", "dev_data")
 # Make sure our directories exist
 [node['dirs']['log'],
  node['dirs']['source'],
+ node['dirs']['ssl'],
  node['vine_shared']['supervisord_log_dir']
 ].each do |dir|
   directory dir do
@@ -40,8 +41,32 @@ if node.chef_environment == "dev"
   end
 end
 
+# Create the SSL certificate and key files
+["crt",
+ "key"
+].each do |type|
+  template "ssl_web.#{type}" do
+    path "#{node['dirs']['ssl']}/ssl_web.#{type}"
+    source "ssl.#{type}.erb"
+    owner "root"
+    group "root"
+    variables :ssl_string => env_data["server"]["web_ssl_#{type}"]
+    mode 0644
+  end
+end
+
+# Create the nginx.conf file
+template "nginx.conf" do
+  path "#{node['nginx']['dir']}/nginx.conf"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables :env_data => env_data
+  notifies :reload, 'service[nginx]'
+end
+
 # Set up the basic supervisor file
-#include_recipe "supervisord"
 template "supervisord.conf" do
   path "/etc/supervisor/supervisord.conf"
   source "supervisord.conf.erb"
@@ -56,3 +81,17 @@ template "supervisord.conf" do
 end
 
 include_recipe "vine_shared::mysql"
+
+# Add commonly-used commands to the bash history (env_data['mysql']['root_password'] is nil in prod, which works perfectly)
+["mysql -u root -p#{env_data['mysql']['root_password']} -h #{env_data['mysql']['host']} -D #{env_data['mysql']['main_name']}",
+ "tail -f #{node['dirs']['log']}/"
+].each do |command|
+  ruby_block "append line to history" do
+    block do
+      file = Chef::Util::FileEdit.new("/home/#{env_data["server"]["user"]}/.bash_history")
+      file.insert_line_if_no_match("/[^\s\S]/", command)  # regex never matches anything
+      file.write_file
+    end
+  end
+end
+
