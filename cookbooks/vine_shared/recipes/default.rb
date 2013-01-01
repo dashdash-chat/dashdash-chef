f310cf740600891dec6711af812545b2ce91dd4d
Steven Lehrburger <lehrburger@gmail.com>
2013-01-01 14:44:17 -0500

    Add dev.vine.im to /etc/hosts in dev environment
    So that I can use the same VM for both web and ejabberd roles.

--- cookbooks/vine_shared/recipes/default.rb
+++ cookbooks/vine_shared/recipes/default.rb
@@ -6,4 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+env_data = data_bag_item("dev_data", "dev_data")
+
+if node.chef_environment == "dev"
+  ruby_block "append entry to /etc/hosts" do
+    block do
+      file = Chef::Util::FileEdit.new("/etc/hosts")
+      file.insert_line_if_no_match("/#{env_data['server']['domain']}/", "\n127.0.0.1\t#{env_data['server']['domain']}\n")
+      file.write_file
+    end
+  end
+end
+
 include_recipe "vine_shared::mysql"
