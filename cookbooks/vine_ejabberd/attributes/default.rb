8a46154630d4efd28aebdfeec711cd400282cb96
Steven Lehrburger <lehrburger@gmail.com>
2013-01-05 08:38:34 -0500

    Fix attributes that I forgot to update

--- cookbooks/vine_ejabberd/attributes/default.rb
+++ cookbooks/vine_ejabberd/attributes/default.rb
@@ -1,6 +1,7 @@
-default['vine_ejabberd']['ejabberd_repo_dir'] = "#{Chef::Environment.load(node.chef_environment).default_attributes['source_dir']}/ejabberd"
-default['vine_ejabberd']['modules_repo_dir']  = "#{Chef::Environment.load(node.chef_environment).default_attributes['source_dir']}/ejabberd-modules"
+default['vine_ejabberd']['ejabberd_repo_dir'] = "#{Chef::Environment.load(node.chef_environment).default_attributes['dirs']['source']}/ejabberd"
+default['vine_ejabberd']['modules_repo_dir']  = "#{Chef::Environment.load(node.chef_environment).default_attributes['dirs']['source']}/ejabberd-modules"
 default['vine_ejabberd']['ejabberd_lib_dir'] = "/lib/ejabberd"
 default['vine_ejabberd']['ejabberd_cfg_dir'] = "/etc/ejabberd"
 default['vine_ejabberd']['ejabberdctl']['retries'] = 6
 default['vine_ejabberd']['ejabberdctl']['retry_delay'] = 5
+#TODO ejabberd logs
