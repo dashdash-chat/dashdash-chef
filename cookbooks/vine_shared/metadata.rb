b4b0465ef9eaf834a66a8e437daca08ca30af6bc
Steven Lehrburger <lehrburger@gmail.com>
2013-01-11 19:31:25 -0500

    Set cookbook versions, specify prod requirements

--- cookbooks/vine_shared/metadata.rb
+++ cookbooks/vine_shared/metadata.rb
@@ -3,11 +3,11 @@ maintainer_email "lehrburger@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures vine_shared"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
 
 #NOTE using specific versions so that I can stay aware of changes in upstream cookbooks
 depends "mysql", "= 2.1.0"
 depends "database", "= 1.3.10"
 depends "nginx", "= 1.1.2"
 depends "supervisor", "= 0.4.0"
-depends "vine_ejabberd", "~> 0.1.0"
+depends "vine_ejabberd", ">= 0.1.0"
