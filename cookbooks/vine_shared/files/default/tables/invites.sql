284bddb1ba6a3f9547217142ebf2de20adebfc51
Steven Lehrburger <lehrburger@gmail.com>
2013-02-04 16:30:00 -0500

    Enforce that invite sender cannot equal ecipient
    I did this a long time ago, but must have forgotten to commit it. It shouldn't affect prod directly though.

--- cookbooks/vine_shared/files/default/tables/invites.sql
+++ cookbooks/vine_shared/files/default/tables/invites.sql
@@ -7,6 +7,18 @@ CREATE TABLE invites (
     FOREIGN KEY (sender) REFERENCES users(id),
     UNIQUE (code),
     UNIQUE KEY recipient (recipient, sender),
+    CHECK (sender != 'recipient'),
+    visible BOOLEAN NOT NULL DEFAULT TRUE,
     used TIMESTAMP DEFAULT 0,
     created TIMESTAMP DEFAULT NOW() ON UPDATE NOW()
 ) ENGINE = InnoDB;
+
+delimiter |
+CREATE TRIGGER check_invite_sender_recipient BEFORE UPDATE ON invites
+FOR EACH ROW BEGIN 
+    IF NEW.recipient = OLD.sender THEN
+        SET NEW.recipient = NULL;
+    END IF;
+END
+|
+delimiter ;
